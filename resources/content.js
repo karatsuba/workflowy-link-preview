8e8f28d750afe3d8e64ae43cdaafc501c6196d93
Roman Chukhan <romanchuhan18@gmail.com>
2019-01-21 23:06:52 +0100

    try to get name content nodes from DOM

--- resources/content.js
+++ resources/content.js
@@ -1,1 +1,11 @@
-console.log('HELLO FROM THIS PAGE');
+const NAME_CONTENT = '.name .content';
+
+const content = () => document.querySelectorAll(NAME_CONTENT);
+
+const items = content();
+
+console.log(document.querySelectorAll(NAME_CONTENT));
+
+setTimeout(() => {
+    console.log(document.querySelectorAll(NAME_CONTENT));
+}, 4000);
