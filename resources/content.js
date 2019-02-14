b48a331a01202a9767ad73c263659eb09be474b8
Roman Chukhan <romanchuhan18@gmail.com>
2019-02-14 18:57:29 +0100

    eject was bad idea, revert it back

--- resources/content.js
+++ resources/content.js
@@ -2,10 +2,20 @@ const NAME_CONTENT = '.name .content';
 
 const content = () => document.querySelectorAll(NAME_CONTENT);
 
-const items = content();
+const parseTarget = (mutation) => {
+    console.log(mutation.target, mutation.target.innerText);
+}
 
-console.log(document.querySelectorAll(NAME_CONTENT));
+const init = () => {
+    var observer = new MutationObserver((mutations, observer) => {
+        // going to send all nodes to react
+        mutations.forEach(parseTarget);
+    });
+    
+    observer.observe(document.getElementById('app'), {
+        childList: true,
+        subtree: true
+    });
+}
 
-setTimeout(() => {
-    console.log(document.querySelectorAll(NAME_CONTENT));
-}, 4000);
+init();
