a4b0d802f7bc17bf6597a34436bfb0c2372d5ca5
Roman Chukhan <romanchuhan18@gmail.com>
2019-02-23 20:52:24 +0100

    copy manifest.json to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
-    DIST: path.resolve(__dirname, "./dist"),
-    SRC_BACKGROUND: path.resolve(__dirname, "./src/background"),
-    SRC_CONTENT: path.resolve(__dirname, "./src/content"),
-    RESOURCES: path.resolve(__dirname, "./resources"),
-    MANIFEST: path.resolve(__dirname, "./resources/manifest.json")
+    DIST: path.resolve(__dirname, './dist'),
+    SRC_BACKGROUND: path.resolve(__dirname, './src/background'),
+    SRC_CONTENT: path.resolve(__dirname, './src/content'),
+    RESOURCES: path.resolve(__dirname, './resources'),
+    MANIFEST: path.resolve(__dirname, './resources/manifest.json')
 };
 
 const getBaseConfig = (options = {}) => ({
     mode: 'development',
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -29,7 +32,8 @@ const background = Object.assign({}, getBaseConfig(), {
     output: {
         filename: 'background.js',
         path: PATHS.DIST
-    }
+    },
+    plugins: [new CopyPlugin([{ from: PATHS.MANIFEST, to: PATHS.DIST }])]
 });
 
 const content = Object.assign({}, getBaseConfig(), {
