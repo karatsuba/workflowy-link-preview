2f991a50c717e9f84e4c86cdf1f2076560ca1136
Roman Chukhan <romanchuhan18@gmail.com>
2019-02-26 00:26:12 +0100

    create store and links state

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,26 +1,50 @@
-import {Observer} from './services/Observer';
-import {Links} from './models/Links';
+import { Observer } from './services/Observer';
+import { Links } from './models/Links';
 
-const target = <HTMLElement> document.getElementById('app');
+import { createStore } from 'redux';
+
+const reducer = (state = { links: Links.create([]) }, action: any): any => {
+    console.log('ACTION', action);
+    switch (action.type) {
+        case 'NEW_LINK':
+            return {
+                links: Links.merge(state.links, action.payload)
+            }
+        default:
+            return state
+    }
+};
+
+const store = createStore(reducer);
 
 const getLinks = (node: Node) => {
-    return (node as HTMLElement).getElementsByClassName("contentLink");
-}
-
-const links = new Links();
+    return Array.from(
+        (node as HTMLElement).getElementsByClassName('contentLink')
+    );
+};
 
 const callback = (mutations: MutationRecord[]) => {
     mutations.forEach(mutation => {
-        links.setLinks(getLinks(mutation.target));
-    })
+        const links = getLinks(mutation.target);
+
+        store.dispatch({
+            type: 'NEW_LINK',
+            payload: Links.create(links)
+        });
+
+        // links.setLinks(targetLinks);
+    });
 };
 
-const observer: Observer = new Observer(target, callback);
+const observer: Observer = new Observer(
+    <HTMLElement>document.getElementById('app'),
+    callback
+);
 
 const createPreviewButtonNode = (target: any) => {
     // if (target.nextSibling && target.nextSibling.tagName !== 'SPAN') {
-        console.log('GOING TO INSERT NODE', target);
-        // const span = document.createElement("span");
-        // target.parentNode.insertBefore(span, target.nextSibling);
+    console.log('GOING TO INSERT NODE', target);
+    // const span = document.createElement("span");
+    // target.parentNode.insertBefore(span, target.nextSibling);
     // }
-}
+};
--- src/content/models/Links.ts
+++ src/content/models/Links.ts
@@ -3,19 +3,29 @@ import { Link } from "./Link";
 export class Links {
     private links: Map<string, Link> = new Map();
 
+    constructor(links: Element[]) {
+        links.map(Link.create)
+            .forEach(link => {
+                this.setLink(link)
+            });
+    }
+
+    static create(links: Element[] = []) {
+        return new Links(links);
+    }
+
+    static merge(oldLinks: Links, newLinks: Links) {
+        const links = new Map<string, Link>([...oldLinks.getLinks(), ...newLinks.getLinks()]);
+        return Links.create().setLinks(links)
+    }
+
     public setLink(link: Link) {
         this.links.set(link.getId(), link);
     }
 
-    public setLinks(links: HTMLCollectionOf<Element>) {
-        Array.from(links)
-            .map(Link.create)
-            .forEach(link => {
-                console.log('LINK', link);
-                this.setLink(link)
-            });
-
-        console.log('LINKS HERE:', this.getLinks());
+    public setLinks(links: Map<string, Link>) {
+        this.links = links;
+        return this;
     }
 
     public getLinks() {
