cf64b9a66d3b31ad27636d4079b7bf378a4c04a0
Roman Chukhan <romanchuhan18@gmail.com>
2019-03-25 22:09:28 +0100

    add empty popup html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
 
 const PATHS = {
     DIST: path.resolve(__dirname, './dist'),
     SRC_BACKGROUND: path.resolve(__dirname, './src/background'),
     SRC_CONTENT: path.resolve(__dirname, './src/content'),
+    SRC_POPUP: path.resolve(__dirname, './src/popup'),
     RESOURCES: path.resolve(__dirname, './resources'),
     MANIFEST: path.resolve(__dirname, './resources/manifest.json')
 };
@@ -30,7 +32,8 @@ const getBaseConfig = (options = {}) => ({
 const config = Object.assign({}, getBaseConfig(), {
     entry: {
         background: path.resolve(PATHS.SRC_BACKGROUND),
-        content: path.resolve(PATHS.SRC_CONTENT)
+        content: path.resolve(PATHS.SRC_CONTENT),
+        popup: path.resolve(PATHS.SRC_POPUP)
     },
     output: {
         filename: '[name].js',
@@ -38,7 +41,11 @@ const config = Object.assign({}, getBaseConfig(), {
     },
     plugins: [
         new ChromeExtensionReloader(),
-        new CopyPlugin([{ from: PATHS.MANIFEST, to: PATHS.DIST }])
+        new CopyPlugin([{ from: PATHS.MANIFEST, to: PATHS.DIST }]),
+        new HtmlWebpackPlugin({
+            filename: 'popup.html',
+            chunks: ['popup']
+        })
     ]
 });
 
